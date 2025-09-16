ffd64a2dae7da433cd44eeeb62fd9af7cd742f2a
rahul122703 <killbusynes@gmail.com>
2025-09-16 22:14:22 +0530

    added redux

--- components/BottomNavMobile.jsx
+++ components/BottomNavMobile.jsx
@@ -2,11 +2,16 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getNavItems } from "../store/uiSlice"; // Import the selector/helper function
 
 export default function BottomNavMobile() {
   const pathname = usePathname();
-  const navItems = useSelector((state) => state.ui.navItems);
+  const state = useSelector((state) => state.ui);
+  const dispatch = useDispatch();
+
+  // Get the nav items dynamically based on state and dispatch
+  const navItems = getNavItems(state, dispatch);
 
   return (
     <div className="fixed bottom-0 left-0 w-full h-[4rem] bg-blue-900 text-white flex justify-around items-center md:hidden z-50 border-t border-blue-700 rounded-tr-2xl rounded-tl-2xl overflow-x-auto no-scrollbar">
@@ -17,10 +22,12 @@ export default function BottomNavMobile() {
         return (
           <Link
             key={idx}
-            href={item.route}
+            href={item.route || "#"} // In case route is undefined (like toggle button)
             className={`flex flex-col items-center text-xs gap-1 min-w-[4rem] ${
               isActive ? "text-white" : "text-blue-300"
-            }`}>
+            }`}
+            onClick={item.isButton ? item.action : undefined} // Handle button actions
+          >
             <Icon className="w-5 h-5" />
             <span className="text-[10px]">{item.label}</span>
           </Link>
--- components/SidebarDesktop.jsx
+++ components/SidebarDesktop.jsx
@@ -2,34 +2,29 @@
 
 import { useSelector, useDispatch } from "react-redux";
 import { usePathname } from "next/navigation";
-import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
 import NavList from "./NavList";
 import LogoutButton from "./LogoutButton";
-import { toggleCollapse } from "../store/uiSlice";
+import { getNavItems } from "../store/uiSlice"; // Import the selector/helper function
 
 export default function SidebarDesktop() {
   const pathname = usePathname();
   const dispatch = useDispatch();
-  const collapsed = useSelector((state) => state.ui.collapsed);
-  const items = useSelector((state) => state.ui.navItems);
+  const state = useSelector((state) => state.ui);
 
-  const navItems = [
-    {
-      label: collapsed ? "Expand Sidebar" : "Collapse Sidebar",
-      icon: collapsed ? PanelLeftOpen : PanelLeftClose,
-      action: () => dispatch(toggleCollapse()),
-      isButton: true,
-    },
-    ...items,
-  ];
+  // Get the nav items using the state and dispatch
+  const navItems = getNavItems(state, dispatch);
 
   return (
     <div
       className={`hidden md:flex h-screen text-white p-4 flex-col justify-between bg-transparent transition-all duration-300 ${
-        collapsed ? "w-20" : "w-60"
+        state.collapsed ? "w-20" : "w-60"
       }`}>
-      <NavList items={navItems} pathname={pathname} collapsed={collapsed} />
-      <LogoutButton collapsed={collapsed} />
+      <NavList
+        items={navItems}
+        pathname={pathname}
+        collapsed={state.collapsed}
+      />
+      <LogoutButton collapsed={state.collapsed} />
     </div>
   );
 }
