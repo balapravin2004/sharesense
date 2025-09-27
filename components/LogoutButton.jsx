686dcef81d7089e778d3b685ff92a004b77895b4
rahul122703 <killbusynes@gmail.com>
2025-09-27 23:06:24 +0530

    login logout button showup logic

--- components/LogoutButton.jsx
+++ components/LogoutButton.jsx
@@ -1,21 +1,27 @@
 "use client";
 
 import React from "react";
-import toast from "react-hot-toast";
 import { LogOut } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { logoutUser } from "../store/authSlice";
-const LogoutButton = ({ collapsed, isMobile }) => {
+
+const LogoutButton = ({ collapsed, isMobile, isActive }) => {
   const dispatch = useDispatch();
+
+  const baseClasses =
+    "flex items-center gap-3 py-3 px-4 rounded-xl transition-colors";
+  const activeClasses =
+    "bg-gradient-to-r from-blue-600 to-blue-500 text-white shadow-lg";
+  const inactiveClasses = "hover:bg-blue-800 text-gray-300";
+
   return (
     <button
       onClick={() => dispatch(logoutUser())}
-      className={`flex ${
-        isMobile
-          ? "flex-col text-xs gap-1 text-blue-300 hover:text-white transition"
-          : "items-center gap-3 py-3 px-4 rounded-xl hover:bg-blue-800 text-gray-300 transition"
+      className={`md:w-full border border-none ${baseClasses} ${
+        isActive ? activeClasses : inactiveClasses
       }`}>
       <LogOut className="w-5 h-5 flex-shrink-0" />
+
       {!collapsed && !isMobile && <span className="font-medium">Logout</span>}
     </button>
   );
