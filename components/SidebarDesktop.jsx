1436a51fa6fad5c650218e59ff9c85f9ca4b1289
rahul122703 <killbusynes@gmail.com>
2025-11-01 23:02:08 +0530

    adding QRcode to the sidebar

--- components/SidebarDesktop.jsx
+++ components/SidebarDesktop.jsx
@@ -3,26 +3,37 @@
 import { useSelector, useDispatch } from "react-redux";
 import { usePathname } from "next/navigation";
 import NavList from "./NavList";
-import { getNavItems } from "../store/uiSlice"; // Import the selector/helper function
+import { getNavItems } from "../store/uiSlice";
 
 export default function SidebarDesktop() {
   const pathname = usePathname();
   const dispatch = useDispatch();
   const state = useSelector((state) => state.ui);
 
-  // Get the nav items using the state and dispatch
   const navItems = getNavItems(state, dispatch);
 
   return (
     <div
-      className={`hidden md:flex h-screen text-white p-4 flex-col justify-between bg-transparent transition-all duration-300 ${
+      className={`hidden md:flex h-screen text-white p-4 flex-col justify-between bg-transparent transition-all duration-300 border-none ${
         state.collapsed ? "w-20" : "w-60"
       }`}>
-      <NavList
-        items={navItems}
-        pathname={pathname}
-        collapsed={state.collapsed}
-      />
+      <div className="max-h-[50rem] h-full flex flex-col justify-between items-center w-full border border-none">
+        <div className="flex-1 w-full">
+          <NavList
+            items={navItems}
+            pathname={pathname}
+            collapsed={state.collapsed}
+          />
+        </div>
+
+        <div className="w-full h-40 mt-4 overflow-hidden border border-none">
+          <img
+            src="/media/images/QRcode.png"
+            alt="QR Code"
+            className="w-full h-full object-contain rounded-xl border border-none"
+          />
+        </div>
+      </div>
     </div>
   );
 }
