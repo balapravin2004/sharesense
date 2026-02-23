d85cff4b6135e5c6241ac9e99d14115c4101bbd2
rahul122703 <killbusyness@gmail.com>
2026-02-23 14:47:52 +0530

    added new contract

--- app/SecureSharePage/page.jsx
+++ app/SecureSharePage/page.jsx
@@ -24,7 +24,7 @@ export default function Page() {
         const address = await signer.getAddress();
         setAccount(address);
 
-        const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+        const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
         const contract = new ethers.Contract(
           contractAddress,
           Upload.abi,
